256d8de9e192c6171d18b5269ad6ed5c90dae697
LuuBao5197 <baolpb5197@gmail.com>
2024-12-28 23:12:54 +0700

    LUU BAO first create StaffController

---
+++ eProject/Controllers/StaffController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        private readonly DatabaseContext _dbContext;
+
+        public StaffController(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("GetAllStudent")]
+        public async Task<IActionResult> GetAllStudent()
+        {
+            var students = await _dbContext.Students.ToListAsync();
+            return Ok(students);
+        }
+        [HttpGet("GetDetailStudent/{id}")]
+        public async Task<IActionResult> GetDetailStudent(int id) { 
+            var student = await _dbContext.Students.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(student);
+        }
+    }
+}
