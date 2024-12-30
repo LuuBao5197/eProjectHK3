5d9c8d0ecbb7e2adb8a56efc21dff8f088433fcb
Minh Quan <150663837+mrorabang@users.noreply.github.com>
2024-12-30 16:17:03 +0700

    ok

--- eProject/Controllers/ManagerController.cs
+++ eProject/Controllers/ManagerController.cs
@@ -63,7 +63,6 @@ namespace eProject.Controllers
             return Ok(awards);
         }
 
-        [HttpGet("GetAwardDetail/{id}")]
         //Method Get Detail of the prizes that have been awarded By Id
         [HttpGet("GetAwardDetail/{id}")]
 
@@ -77,7 +76,6 @@ namespace eProject.Controllers
             return Ok(award);
         }
 
-        [HttpGet("GetAllSubmissions")]
         //Method Get All Submissions
         [HttpGet("GetAllSubmissions")]
 
@@ -91,7 +89,6 @@ namespace eProject.Controllers
             return Ok(submissions);
         }
 
-        [HttpGet("GetAllExhibitionArtwork")]
         //Method Get all artwork that have been send to exhibition
         [HttpGet("GetAllExhibitionArtwork")]
 
