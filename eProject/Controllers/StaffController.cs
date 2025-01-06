a7a965e5e8a5be83bcbe5dbb51b9999abb438093
LuuBao5197 <baolpb5197@gmail.com>
2025-01-06 20:39:55 +0700

    Update API Staff feature Award

--- eProject/Controllers/StaffController.cs
+++ eProject/Controllers/StaffController.cs
@@ -28,46 +28,6 @@ namespace eProject.Controllers
             return Ok(student);
         }
 
-        //API RELATED CONTEST
-        /*        [HttpGet("GetAllContest")]
-                public async Task<IActionResult> GetAllContest(int page = 1, int pageSize = 6, string? search = null)
-                {
-                    var query = _dbContext.Contests.AsQueryable();
-
-                    if (!string.IsNullOrEmpty(search))
-                    {
-                        query = query.Where(c => c.Name.Contains(search));
-                    }
-
-                    if (page <= 0 || pageSize <= 0)
-                    {
-                        return BadRequest("Page and pageSize must be greater than 0.");
-                    }
-
-                    // Tính toán số lượng mục bỏ qua (skip)
-                    var skip = (page - 1) * pageSize;
-
-                    // Lấy tổng số lượng contests
-                    var totalItems = await _dbContext.Contests.CountAsync();
-
-                    // Lấy dữ liệu phân trang
-                    var contests = await query
-                        .OrderBy(c => c.Id) // Sắp xếp theo ID (hoặc tiêu chí phù hợp)
-                        .Skip(skip)
-                        .Take(pageSize)
-                        .ToListAsync();
-
-                    // Trả về dữ liệu cùng thông tin phân trang
-                    var response = new
-                    {
-                        contests = contests,
-                        totalItems = totalItems,
-                        totalPages = (int)Math.Ceiling(totalItems / (double)pageSize),
-                        currentPage = page
-                    };
-
-                    return Ok(response);
-                }*/
 
         [HttpGet("GetAllContest")]
         public async Task<IActionResult> GetAllContest(int page = 1, int pageSize = 10, string? search = null)
@@ -92,8 +52,6 @@ namespace eProject.Controllers
                 totalItems
             });
         }
-
-
 
         [HttpGet("GetDetailContest/{id}")]
         public async Task<IActionResult> GetDetailContest(int id)
@@ -137,6 +95,7 @@ namespace eProject.Controllers
             }
         }
 
+
         [HttpDelete("DeleteContest/{id}")]
         public  async Task<IActionResult> DeleteContest(int id)
         {
@@ -155,6 +114,64 @@ namespace eProject.Controllers
            
         }
 
-        
+        [HttpGet("GetAllAward")]
+        public async Task<IActionResult> GetAllAward(int page = 1, int pageSize = 10, string? search = null)
+        {
+            var query = _dbContext.Awards.AsQueryable();
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                query = query.Where(c => c.Name.Contains(search));
+            }
+
+            var totalItems = await query.CountAsync();
+            var awards = await query
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return Ok(new
+            {
+                awards,
+                totalPages = (int)Math.Ceiling(totalItems / (double)pageSize),
+                totalItems
+            });
+        }
+
+        [HttpPost("AddAward")]
+        public async Task<IActionResult> AddAward(Award award)
+        {
+            await _dbContext.Awards.AddAsync(award);
+            await _dbContext.SaveChangesAsync();
+            return Ok(award);
+        }
+
+        [HttpPut("EditAward/{id}")]
+        public async Task<IActionResult> EditAward(int id, Award award)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(new { message = "Data is not valid" });
+                }
+
+                if (id != award.Id)
+                {
+                    return NotFound(new { message = "No result about this contest" });
+                }
+
+                _dbContext.Entry(award).State = EntityState.Modified;
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(new { message = "Contest updated successfully", data = award });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message });
+
+            }
+        }
+
     }
 }
